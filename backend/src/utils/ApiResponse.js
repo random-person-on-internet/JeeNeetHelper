8456289e08bbcb44ca39bfd0787af9cf2f284356
random-person-on-internet <vedlakkad05@gmail.com>
2024-04-26 17:05:16 +0530

    Utils-ApiResponse.js updated

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -3,7 +3,9 @@ class ApiResponse {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
-    this.success = statusCode<400;
+    this.success = statusCode < 400;
     // if less than 400, its a true or else its error
   }
 }
+
+export default ApiResponse;
